cc5d4cca24d76f9061533b3f2528b1349267a34b
umgonji <0deepsleep@gmail.com>
2020-05-23 12:36:08 +0900

    Initial commit

---
+++ app/src/androidTest/java/dasdsa/sdn/uipractice_20200523/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package dasdsa.sdn.uipractice_20200523
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("dasdsa.sdn.uipractice_20200523", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/dasdsa/sdn/uipractice_20200523/MainActivity.kt
@@ -0,0 +1,12 @@
+package dasdsa.sdn.uipractice_20200523
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
